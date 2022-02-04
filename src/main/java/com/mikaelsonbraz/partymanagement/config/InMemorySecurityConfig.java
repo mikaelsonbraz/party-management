874fedfaedf35aded1bae717d7520116aa3379e8
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-02-04 19:31:41 -0300

    voltando1

--- src/main/java/com/mikaelsonbraz/partymanagement/config/InMemorySecurityConfig.java
+++ src/main/java/com/mikaelsonbraz/partymanagement/config/InMemorySecurityConfig.java
@@ -1,13 +1,10 @@
 package com.mikaelsonbraz.partymanagement.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class InMemorySecurityConfig {
 
     @Autowired
